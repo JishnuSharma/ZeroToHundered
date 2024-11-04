7076df8a7dc8e3bd9de9ba66aca7c327ac936732
JishnuSharma <jishnusharma18@gmail.com>
2024-11-04 20:03:12 +0530

    Recoil in counter app for state management along with selectors

--- Zero To One/Week 7/RecoilProject/src/App.jsx
+++ Zero To One/Week 7/RecoilProject/src/App.jsx
@@ -2,15 +2,59 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { countAtom, eventSelector } from './store/count'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <RecoilRoot>
+                <Count></Count>
+            </RecoilRoot>
+        </>
+    )
+}
 
-  return (
-    <>
-      
-    </>
-  )
+function Count() {
+
+    return (
+        <>
+            <CountRenderer></CountRenderer>
+            <Buttons></Buttons>
+        </>
+    )
+}
+
+function CountRenderer() {
+    const count = useRecoilValue(countAtom);
+
+    return (
+        <>
+            {count}
+            <IsEven></IsEven>
+        </>
+    )
+}
+
+function Buttons() {
+    const setCount = useSetRecoilState(countAtom);
+
+    return (
+        <>
+            <button onClick={() => setCount((count) => count + 1)} >Increase</button>
+            <button onClick={() => setCount((count) => count - 1)}>Decrease</button>
+        </>
+    )
+}
+
+function IsEven() {
+    const isEven = useRecoilValue(eventSelector);
+
+    return (
+        <>
+            {isEven ? "It is an even number" : null}
+        </>
+    )
 }
 
 export default App
